2711c244579748c2d9e4fe790ea008472db3424c
Ben Badcock <Ben Badcock>
2016-07-13 08:47:02 +0100

    Binding up colour picker to Tank Aiming component to change colour based on enum status

--- BattleTanks/Source/BattleTanks/TankPlayerController.h
+++ BattleTanks/Source/BattleTanks/TankPlayerController.h
@@ -15,15 +15,15 @@ class BATTLETANKS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
-public:
+protected:
+	UFUNCTION(BlueprintCallable, Category = "Setup")
 	ATank* GetControlledTank() const;
-	
+
+private:
 	virtual void BeginPlay() override;
 
 	virtual void Tick(float DeltaSeconds) override;
 
-
-private:
 	// Star the tank moving the barrel so that a shot would hit where
 	// the crosshair intersects the world
 	void AimTowardsCrosshair();
