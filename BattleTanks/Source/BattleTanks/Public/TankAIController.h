52ca167980d872e914f0fcf4c77c4e2e36f9d9e1
Ben Badcock <Ben Badcock>
2016-07-24 10:12:48 +0100

    Fixed distance that AI can move to

--- BattleTanks/Source/BattleTanks/Public/TankAIController.h
+++ BattleTanks/Source/BattleTanks/Public/TankAIController.h
@@ -16,10 +16,12 @@ class BATTLETANKS_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+protected:
+	// How close can the tank get to the player
+	UPROPERTY(EditDefaultsOnly, Category = "Movement")
+	float AcceptanceRadius = 8000.0f;
+
 private:
 	virtual void BeginPlay() override;	
 	virtual void Tick(float DeltaSeconds) override;
-
-	// How close can the tank get to the player
-	float AcceptanceRadius = 3000.0f;
 };
